69bfb123f754efff69b2f9723061ae021faeacc7
francois-larcin <francoislarcin@hotmail.com>
2025-08-21 14:33:27 +0200

    fav-stock.controller

---
+++ backend/src/controllers/fav-stock.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { SessionUser } from 'src/decorators/session.decorator';
+import { AddDeleteFavoriteDto } from 'src/dtos/favorites/add-fav.dto';
+import { FavStockService } from 'src/services/fav-stock.service';
+
+@Controller('favorites/lists/:listId/stocks')
+export class FavStockController {
+  constructor(private readonly stocks: FavStockService) {}
+
+  //? Ajouter une action (symbol dans le body)
+
+  @Post()
+  async add(
+    @SessionUser('id') userId: string,
+    @Param('listId', ParseIntPipe) listId: number,
+    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    dto: AddDeleteFavoriteDto,
+  ) {
+    return this.stocks.addStockToList(userId, listId, dto);
+  }
+
+  //? Retirer une action (symbol dans le body)
+
+  @Delete()
+  async remove(
+    @SessionUser('id') userId: string,
+    @Param('listId', ParseIntPipe) listId: number,
+    @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
+    dto: AddDeleteFavoriteDto,
+  ) {
+    return this.stocks.removeStockFromList(userId, listId, dto);
+  }
+}
