7a9f430fe43cc9424abf593cff666e0be32fb101
francois-larcin <francoislarcin@hotmail.com>
2025-08-21 16:52:50 +0200

    rendre variable env visible dans auth.module

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from 'src/controllers/auth.controller';
@@ -9,12 +10,20 @@ import { UserService } from 'src/services/user.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev', // Assure-toi que ce chemin est correct
+      isGlobal: true,
+    }),
     TypeOrmModule.forFeature([UserEntity, RoleEntity]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
     }),
   ],
+
   controllers: [UserController],
   providers: [UserService, AuthMiddleware],
   exports: [UserService, AuthMiddleware, JwtModule],
